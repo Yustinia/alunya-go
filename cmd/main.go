3a2710dab85c0df1c3f4a56ca01351ea29e662ac
Yustinia <yustinia@protonmail.com>
2026-08-11 23:36:06 +0800

    placeholder main.go content & tidy gopaper

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"os"
+
+	"github.com/Yustinia/gopaper"
+)
+
+var (
+	API_KEY = os.Getenv("WALLHAVEN_API_KEY")
+	CLIENT  = gopaper.NewClient(API_KEY)
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	params := gopaper.NewSearch()
+	params.KeySearch = "japan"
+
+	result, err := CLIENT.Search(params)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	for i, wall := range result.Wallpapers {
+		log.Println(i, wall.Path)
+	}
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/Yustinia/alunya-go
 
 go 1.26.5
+
+require github.com/Yustinia/gopaper v0.11.0
