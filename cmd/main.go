a0791e0e271fc2db82038517a399826804594e5e
Yustinia <yustinia@protonmail.com>
2026-08-16 17:24:56 +0800

    testing binary

--- cmd/main.go
+++ cmd/main.go
@@ -17,16 +17,39 @@ var (
 	CLIENT  = gopaper.NewClient(API_KEY)
 )
 
+// func main() {
+// 	params := gopaper.NewSearch()
+// 	params.KeySearch = "japan"
+//
+// 	result, err := CLIENT.Search(params)
+// 	if err != nil {
+// 		log.Fatalln(err)
+// 	}
+//
+// 	for i, wall := range result.Wallpapers {
+// 		log.Println(i, wall.Path)
+// 	}
+// }
+
 func main() {
-	params := gopaper.NewSearch()
-	params.KeySearch = "japan"
+	a := app.NewWithID("alunya")
+	w := a.NewWindow("alunya")
 
-	result, err := CLIENT.Search(params)
-	if err != nil {
-		log.Fatalln(err)
-	}
+	searchEntry := widget.NewEntry()
+	searchEntry.SetPlaceHolder("Search")
 
-	for i, wall := range result.Wallpapers {
-		log.Println(i, wall.Path)
-	}
+	generalCatTog := widget.NewCheck("General", func(b bool) {
+		log.Printf("Genearl: %v\n", b)
+	})
+
+	sfwPurTog := widget.NewCheck("SFW", func(b bool) {
+		log.Printf("SFW: %v\n", b)
+	})
+
+	topR := container.NewHBox(generalCatTog, sfwPurTog)
+	tester := container.NewVBox(searchEntry, topR)
+
+	w.SetContent(tester)
+	w.Resize(fyne.NewSize(300, 200))
+	w.ShowAndRun()
 }
