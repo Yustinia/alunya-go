225d95c1310479bd1c0951bb321f7e2f8776b707
Yustinia <yustinia@protonmail.com>
2026-08-17 21:33:33 +0800

    search section visual

--- cmd/main.go
+++ cmd/main.go
@@ -35,53 +35,20 @@ func main() {
 	a := app.NewWithID("alunya")
 	w := a.NewWindow("alunya")
 
-	searchEntry := widget.NewEntry()
-	searchEntry.SetPlaceHolder("Enter Search")
-
-	sfw := widget.NewCheck("SFW", func(b bool) {
-		log.Println("SFW:", b)
-	})
-	sketchy := widget.NewCheck("Sketchy", func(b bool) {
-		log.Println("Sketchy:", b)
-	})
-	nsfw := widget.NewCheck("NSFW", func(b bool) {
-		log.Println("NSFW:", b)
-	})
-	purityRow := container.NewHBox(sfw, sketchy, nsfw)
-
-	general := widget.NewCheck("General", func(b bool) {
-		log.Println("General:", b)
-	})
-	anime := widget.NewCheck("Anime", func(b bool) {
-		log.Println("Anime:", b)
-	})
-	people := widget.NewCheck("People", func(b bool) {
-		log.Println("People:", b)
-	})
-	categoryRow := container.NewHBox(general, anime, people)
-
-	sortDropDown := widget.NewSelect([]string{"Relevance", "Date Added", "Random"}, func(s string) {
-		log.Println("Sort:", s)
+	queryEntry := widget.NewEntry()
+	queryEntry.SetPlaceHolder("This is your search")
+	queryForm := widget.NewForm(
+		widget.NewFormItem("Search", queryEntry),
+	)
+	querySearchButton := widget.NewButton("Enter", func() {
+		log.Printf("Searched: %v\n", queryEntry.Text)
 	})
 
-	catPurSortRow := container.NewHBox(categoryRow, purityRow, sortDropDown)
+	searchContainer := container.NewBorder(nil, nil, nil, querySearchButton, queryForm)
 
-	aspectRatio := widget.NewSelect([]string{"Any", "All Wide", "16:9"}, func(s string) {
-		log.Println("Ratio:", s)
-	})
-	resolution := widget.NewSelect([]string{"At Least", "Exact"}, func(s string) {
-		log.Println("Resolution:", s)
-	})
-	resXAxis := widget.NewEntry()
-	resXAxis.SetPlaceHolder("1920")
-	resYAxis := widget.NewEntry()
-	resYAxis.SetPlaceHolder("1080")
+	windowContainer := container.NewVBox(searchContainer)
 
-	ratResXYRow := container.NewHBox(aspectRatio, resolution, resXAxis, resYAxis)
-
-	filterSection := container.NewVBox(searchEntry, catPurSortRow, ratResXYRow)
-
-	w.SetContent(filterSection)
+	w.SetContent(windowContainer)
 	w.Resize(fyne.NewSize(300, 200))
 	w.ShowAndRun()
 }
