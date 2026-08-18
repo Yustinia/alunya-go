0d95f028feb7463cc94363480f5b26618655c1ae
Yustinia <yustinia@protonmail.com>
2026-08-18 14:35:03 +0800

    refactor for modularity

--- cmd/main.go
+++ cmd/main.go
@@ -1,15 +1,14 @@
 package main
 
 import (
-	"log"
 	"os"
 
+	"github.com/Yustinia/alunya-go/internal/modular"
 	"github.com/Yustinia/gopaper"
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/container"
-	"fyne.io/fyne/v2/widget"
 )
 
 var (
@@ -31,92 +30,12 @@ var (
 // 	}
 // }
 
-func buildSearchSection() *fyne.Container {
-	queryEntry := widget.NewEntry()
-	queryEntry.SetPlaceHolder("This is your search")
-	queryForm := widget.NewForm(
-		widget.NewFormItem("Search", queryEntry),
-	)
-	querySearchButton := widget.NewButton("Enter", func() {
-		log.Printf("Searched: %v\n", queryEntry.Text)
-	})
-
-	searchContainer := container.NewBorder(nil, nil, nil, querySearchButton, queryForm)
-
-	return searchContainer
-}
-
-func buildCategorySection() *fyne.Container {
-	generalToggle := widget.NewCheck("General", func(b bool) {
-		log.Printf("General: %v\n", b)
-	})
-	animeToggle := widget.NewCheck("Anime", func(b bool) {
-		log.Printf("Anime: %v\n", b)
-	})
-	peopleToggle := widget.NewCheck("People", func(b bool) {
-		log.Printf("People: %v\n", b)
-	})
-
-	categoryContainer := container.NewGridWithColumns(3, generalToggle, animeToggle, peopleToggle)
-
-	return categoryContainer
-}
-
-func buildPuritySection() *fyne.Container {
-	sfwToggle := widget.NewCheck("SFW", func(b bool) {
-		log.Printf("SFW: %v\n", b)
-	})
-	sketchyToggle := widget.NewCheck("Sketchy", func(b bool) {
-		log.Printf("Sketchy: %v\n", b)
-	})
-	nsfwToggle := widget.NewCheck("NSFW", func(b bool) {
-		log.Printf("NSFW: %v\n", b)
-	})
-
-	purityContainer := container.NewGridWithColumns(3, sfwToggle, sketchyToggle, nsfwToggle)
-
-	return purityContainer
-}
-
-func buildSortSection() *widget.Select {
-	entries := []string{
-		"Relevance",
-		"Random",
-		"Date Added",
-		"Views",
-		"Favorites",
-		"Toplist",
-		"Hot",
-	}
-	sortDropDown := widget.NewSelect(entries, func(s string) {
-		log.Printf("Sort: %v\n", s)
-	})
-
-	return sortDropDown
-}
-
-func buildFilterRow() *fyne.Container {
-	categoryForm := formItem("Category", buildCategorySection())
-	purityForm := formItem("Purity", buildPuritySection())
-	sortForm := formItem("Sort", buildSortSection())
-
-	filterRow := container.NewGridWithColumns(3, categoryForm, purityForm, sortForm)
-
-	return filterRow
-}
-
-func formItem(label string, container fyne.CanvasObject) *widget.Form {
-	return widget.NewForm(
-		widget.NewFormItem(label, container),
-	)
-}
-
 func main() {
 	a := app.NewWithID("alunya")
 	w := a.NewWindow("alunya")
 
-	searchContainer := buildSearchSection()
-	filterRow := buildFilterRow()
+	searchContainer := modular.BuildSearchSection()
+	filterRow := modular.BuildFilterRow()
 
 	windowContainer := container.NewVBox(searchContainer, filterRow)
 
---
+++ internal/modular/elements.go
@@ -0,0 +1,89 @@
+package modular
+
+import (
+	"log"
+
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/widget"
+)
+
+func BuildSearchSection() *fyne.Container {
+	queryEntry := widget.NewEntry()
+	queryEntry.SetPlaceHolder("This is your search")
+	queryForm := widget.NewForm(
+		widget.NewFormItem("Search", queryEntry),
+	)
+	querySearchButton := widget.NewButton("Enter", func() {
+		log.Printf("Searched: %v\n", queryEntry.Text)
+	})
+
+	searchContainer := container.NewBorder(nil, nil, nil, querySearchButton, queryForm)
+
+	return searchContainer
+}
+
+func buildCategorySection() *fyne.Container {
+	generalToggle := widget.NewCheck("General", func(b bool) {
+		log.Printf("General: %v\n", b)
+	})
+	animeToggle := widget.NewCheck("Anime", func(b bool) {
+		log.Printf("Anime: %v\n", b)
+	})
+	peopleToggle := widget.NewCheck("People", func(b bool) {
+		log.Printf("People: %v\n", b)
+	})
+
+	categoryContainer := container.NewGridWithColumns(3, generalToggle, animeToggle, peopleToggle)
+
+	return categoryContainer
+}
+
+func buildPuritySection() *fyne.Container {
+	sfwToggle := widget.NewCheck("SFW", func(b bool) {
+		log.Printf("SFW: %v\n", b)
+	})
+	sketchyToggle := widget.NewCheck("Sketchy", func(b bool) {
+		log.Printf("Sketchy: %v\n", b)
+	})
+	nsfwToggle := widget.NewCheck("NSFW", func(b bool) {
+		log.Printf("NSFW: %v\n", b)
+	})
+
+	purityContainer := container.NewGridWithColumns(3, sfwToggle, sketchyToggle, nsfwToggle)
+
+	return purityContainer
+}
+
+func buildSortSection() *widget.Select {
+	entries := []string{
+		"Relevance",
+		"Random",
+		"Date Added",
+		"Views",
+		"Favorites",
+		"Toplist",
+		"Hot",
+	}
+	sortDropDown := widget.NewSelect(entries, func(s string) {
+		log.Printf("Sort: %v\n", s)
+	})
+
+	return sortDropDown
+}
+
+func BuildFilterRow() *fyne.Container {
+	categoryForm := formItem("Category", buildCategorySection())
+	purityForm := formItem("Purity", buildPuritySection())
+	sortForm := formItem("Sort", buildSortSection())
+
+	filterRow := container.NewGridWithColumns(3, categoryForm, purityForm, sortForm)
+
+	return filterRow
+}
+
+func formItem(label string, container fyne.CanvasObject) *widget.Form {
+	return widget.NewForm(
+		widget.NewFormItem(label, container),
+	)
+}
