6ed37249191d3c66128d5c79116d0f05d4ef889a
Yustinia <yustinia@protonmail.com>
2026-08-20 21:07:34 +0800

    add initial state check for first launch on filterTop

--- internal/elements/filterTop.go
+++ internal/elements/filterTop.go
@@ -13,12 +13,17 @@ func buildCategorySection(params *gopaper.SearchParams) *fyne.Container {
 	generalToggle := widget.NewCheck("General", func(b bool) {
 		params.Categories.General = b
 	})
+	generalToggle.SetChecked(params.Categories.General)
+
 	animeToggle := widget.NewCheck("Anime", func(b bool) {
 		params.Categories.Anime = b
 	})
+	animeToggle.SetChecked(params.Categories.Anime)
+
 	peopleToggle := widget.NewCheck("People", func(b bool) {
 		params.Categories.People = b
 	})
+	peopleToggle.SetChecked(params.Categories.People)
 
 	categoryContainer := container.NewGridWithColumns(3, generalToggle, animeToggle, peopleToggle)
 
@@ -29,12 +34,17 @@ func buildPuritySection(params *gopaper.SearchParams) *fyne.Container {
 	sfwToggle := widget.NewCheck("SFW", func(b bool) {
 		params.Purity.SFW = b
 	})
+	sfwToggle.SetChecked(params.Purity.SFW)
+
 	sketchyToggle := widget.NewCheck("Sketchy", func(b bool) {
 		params.Purity.Sketchy = b
 	})
+	sketchyToggle.SetChecked(params.Purity.Sketchy)
+
 	nsfwToggle := widget.NewCheck("NSFW", func(b bool) {
 		params.Purity.NSFW = b
 	})
+	nsfwToggle.SetChecked(params.Purity.NSFW)
 
 	purityContainer := container.NewGridWithColumns(3, sfwToggle, sketchyToggle, nsfwToggle)
 
