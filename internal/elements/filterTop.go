94eceee1a9af0e5ff72ee4a116c6141228d85b29
Yustinia <yustinia@protonmail.com>
2026-08-20 21:01:06 +0800

    add params pointer to categories & purity

--- internal/elements/filterTop.go
+++ internal/elements/filterTop.go
@@ -6,17 +6,18 @@ import (
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/widget"
+	"github.com/Yustinia/gopaper"
 )
 
-func buildCategorySection() *fyne.Container {
+func buildCategorySection(params *gopaper.SearchParams) *fyne.Container {
 	generalToggle := widget.NewCheck("General", func(b bool) {
-		log.Printf("General: %v\n", b)
+		params.Categories.General = b
 	})
 	animeToggle := widget.NewCheck("Anime", func(b bool) {
-		log.Printf("Anime: %v\n", b)
+		params.Categories.Anime = b
 	})
 	peopleToggle := widget.NewCheck("People", func(b bool) {
-		log.Printf("People: %v\n", b)
+		params.Categories.People = b
 	})
 
 	categoryContainer := container.NewGridWithColumns(3, generalToggle, animeToggle, peopleToggle)
@@ -24,15 +25,15 @@ func buildCategorySection() *fyne.Container {
 	return categoryContainer
 }
 
-func buildPuritySection() *fyne.Container {
+func buildPuritySection(params *gopaper.SearchParams) *fyne.Container {
 	sfwToggle := widget.NewCheck("SFW", func(b bool) {
-		log.Printf("SFW: %v\n", b)
+		params.Purity.SFW = b
 	})
 	sketchyToggle := widget.NewCheck("Sketchy", func(b bool) {
-		log.Printf("Sketchy: %v\n", b)
+		params.Purity.Sketchy = b
 	})
 	nsfwToggle := widget.NewCheck("NSFW", func(b bool) {
-		log.Printf("NSFW: %v\n", b)
+		params.Purity.NSFW = b
 	})
 
 	purityContainer := container.NewGridWithColumns(3, sfwToggle, sketchyToggle, nsfwToggle)
@@ -57,9 +58,9 @@ func buildSortSection() *widget.Select {
 	return sortDropDown
 }
 
-func BuildFilterRowTop() *fyne.Container {
-	categoryForm := formItem("Category", buildCategorySection())
-	purityForm := formItem("Purity", buildPuritySection())
+func BuildFilterRowTop(params *gopaper.SearchParams) *fyne.Container {
+	categoryForm := formItem("Category", buildCategorySection(params))
+	purityForm := formItem("Purity", buildPuritySection(params))
 	sortForm := formItem("Sort", buildSortSection())
 
 	filterRow := container.NewGridWithColumns(3, categoryForm, purityForm, sortForm)
