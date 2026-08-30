e5d620fd11c0cddcb8a6c7f3c1c12bf80e28e5d0
Yustinia <yustinia@protonmail.com>
2026-08-30 11:23:41 +0800

    gallery grid per wall border

--- internal/elements/gridGallery.go
+++ internal/elements/gridGallery.go
@@ -1,6 +1,7 @@
 package elements
 
 import (
+	"image/color"
 	"log"
 
 	"fyne.io/fyne/v2"
@@ -12,6 +13,13 @@ import (
 )
 
 func GalleryGrid() (*container.Scroll, func([]gopaper.Wallpaper)) {
+	green := color.RGBA{G: 255, A: 255}
+	red := color.RGBA{R: 255, A: 255}
+	yellow := color.RGBA{R: 255, G: 255, A: 255}
+	size := fyne.NewSize(341, 192)
+	borderSz := 4
+	borderRad := 8
+
 	thumbCache := make(map[string]fyne.Resource)
 
 	var wallpapers []gopaper.Wallpaper
@@ -21,14 +29,40 @@ func GalleryGrid() (*container.Scroll, func([]gopaper.Wallpaper)) {
 
 	createImage := func() fyne.CanvasObject {
 		image := canvas.NewImageFromFile("image/noThumb.jpg")
-		image.FillMode = canvas.ImageFillContain
-		image.SetMinSize(fyne.NewSize(250, 250))
-		return image
+		image.CornerRadius = float32(borderRad)
+
+		bg := canvas.NewRectangle(color.Transparent)
+		bg.StrokeColor = color.RGBA{
+			A: 0,
+		}
+		bg.StrokeWidth = float32(borderSz)
+		bg.CornerRadius = float32(borderRad)
+
+		image.FillMode = canvas.ImageFillCover
+		image.SetMinSize(size)
+		bg.SetMinSize(size)
+
+		stack := container.NewStack(image, bg)
+		return stack
 	}
 
 	updateItem := func(id widget.GridWrapItemID, item fyne.CanvasObject) {
+
 		url := wallpapers[id].ThumbSmall()
-		img := item.(*canvas.Image)
+
+		stack := item.(*fyne.Container)
+		img := stack.Objects[0].(*canvas.Image)
+		bg := stack.Objects[1].(*canvas.Rectangle)
+
+		switch {
+		case wallpapers[id].IsSFW():
+			bg.StrokeColor = green
+		case wallpapers[id].IsSketchy():
+			bg.StrokeColor = yellow
+		case wallpapers[id].IsNSFW():
+			bg.StrokeColor = red
+		}
+		bg.Refresh()
 
 		setImg := func(resource fyne.Resource) {
 			fyne.Do(func() {
